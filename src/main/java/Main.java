e6f9bf8554ac212157df7fcf7a46a03d00587cc5
KostiantynRog <42935404+KostiantynRog@users.noreply.github.com>
2021-01-19 17:23:54 +0200

    Booking (#12)
    * Changed
    
    * changed 19.01.21(2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,4 @@
-import Booking.Booking;
-import Booking.Flight;
-import Booking.Passenger;
-import Booking.Person;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
