3cb9664c038e03300a63c07d7a9f8f29b0d5adf5
krozhkov09@gmail.com <krozhkov09@gmail.com>
2021-01-19 17:02:44 +0200

    changed 19.01.21(2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,8 @@
-import Booking.Booking;
-import Booking.Flight;
-import Booking.Passenger;
-import Booking.Person;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Flight f = new Flight(10, "21/01/2021", "12:00", "London", 10);
-//        Passenger p = new Passenger("Tim", "Roth");
-//        Passenger p1 = new Passenger("Nim", "Dre");
-//
-//         Booking b = new Booking(f, p);
-//         Booking b1 = new Booking(f, p1);
-//         b.createNewPassenger("Qwet", "Adf");
-//
-//        System.out.println(b);
-//        System.out.println(b1);
+
             FlightContext.init();
             Console.start();
     }
